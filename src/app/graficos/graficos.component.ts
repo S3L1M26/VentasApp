f71895dfc1c766726cf12813577c2bf53c3baddf
defmex <def>
2024-11-04 20:51:34 -0300

    configuracion base para graficos en caso de perderla no esta en otra parte

--- src/app/graficos/graficos.component.ts
+++ src/app/graficos/graficos.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-
+import { Chart, registerables } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+Chart.register(...registerables);
 @Component({
   selector: 'app-graficos',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './graficos.component.html',
   styleUrl: './graficos.component.css'
 })
 export class GraficosComponent {
+  public datacount = 7;
+  public NUMBER_CFG = {count: this.datacount, min: -100, max: 100};
 
+  public config: any = {
+    type: 'line',
+    data: {
+      labels : ['Enero', 'Febrero'],
+      datasets: [
+        {
+          label: 'Sales',
+          data: ['467','432', '234','432'],
+          backgroundColor: 'blue',
+        },
+        {
+          label: 'Sales',
+          data: ['500','232', '212','152'],
+          backgroundColor: 'red',
+        },
+      ]
+    },
+    options: {
+      aspectRatio: 1,
+    },
+  };
+  chart: any;
+  ngOnInit():void{
+    this.chart = new Chart('MyChart', this.config);
+  }
 }
