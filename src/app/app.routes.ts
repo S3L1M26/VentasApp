3ed29de105cbe26aff400a42691700fccfbc2fe1
defmex <def>
2024-11-04 19:27:07 -0300

    Creacion de componenete de graficos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,12 @@ import { VistaPedidosComponent } from './vista-pedidos/vista-pedidos.component';
 import { CrearClienteComponent } from './crear-cliente/crear-cliente.component';
 import { CrearComercialComponent } from './crear-comercial/crear-comercial.component';
 import { CrearPedidoComponent } from './crear-pedido/crear-pedido.component';
+import { GraficosComponent } from './graficos/graficos.component';
+
 
 export const routes: Routes = [
     { path: '', component: VistaPedidosComponent },
+    { path: 'graficos', component: GraficosComponent},
     { path: 'crear-cliente', component: CrearClienteComponent },
     { path: 'crear-comercial', component: CrearComercialComponent },
     { path: 'crear-pedido', component: CrearPedidoComponent },
